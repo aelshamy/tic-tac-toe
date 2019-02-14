b8ad6392c758a6674895fe462f6690a315e733ce
elshaa1 <elshaa1@nationwide.com>
2019-02-14 16:17:02 -0500

    clean app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <header>
-          <h1>React App</h1>
-        </header>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          repudiandae distinctio optio culpa, ex debitis est quisquam, dolores
-          explicabo unde ad? Esse corporis odit rerum itaque totam ad inventore
-          voluptate!
-        </p>
+        <h1>Tic Tac Toe</h1>
       </Fragment>
     );
   }
